695dc100e931dcc0c148ea27de78cdbd2534f13f
Aviano-Riccardo <riccardoaviano@libero.it>
2026-05-04 10:00:51 +0200

    Esercitazione verifica

---
+++ Programmazione_in_C/strutture/Esercitazione_04-05-26.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_STUDENTI 20
+#define VOTI 5
+
+typedef struct 
+{
+    char nome[50];
+    char cognome[50];
+    int voti[VOTI];
+    float media;
+} Studenti;
+
+void inserisciStudente (classe[], int n)
+{
+    
+}
